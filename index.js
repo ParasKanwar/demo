a0d673e0f2051a5b3bad698d7ecb77b442e0df32
paras <paraskanwar30@gmail.com>
2020-06-01 14:13:17 +0530

    yo

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-
+require("./database/database");
+const User = require("./models/user/user");
 app.use(express.static(path.join(__dirname, "../demo/build")));
+app.use(express.json());
+app.post("/signup", (req, res) => {
+  const user = req.body;
+  const user1 = new User(user);
+  user1
+    .save()
+    .then((val) => {
+      res.send(val);
+    })
+    .catch((e) => {
+      res.status(500).send(e.message);
+    });
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../demo/build/index.html"));
--- models/user/user.js
+++ models/user/user.js
@@ -5,6 +5,7 @@ const schema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
